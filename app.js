7d81a426f29f217899ea16062f60b9471207f95d
Ajitkumar-25 <kajit0408@gmail.com>
2024-09-09 22:59:06 +0530

    intial commit : intialised Project Backend Folder Structure

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const connectDB = require("./database/db_connection");
+connectDB();
+
+app.listen(process.env.PORT ? process.env.PORT : 5000, () => {
+  console.log(`app listening on PORT ${process.env.PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
