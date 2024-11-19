1808ea17053b486ef68d6d90956ed56892b2c63c
Ajitkumar-25 <kajit0408@gmail.com>
2024-11-19 21:43:28 +0530

    modified Footer

--- src/LandingPage/components/Footer.jsx
+++ src/LandingPage/components/Footer.jsx
@@ -1,11 +1,21 @@
-import React from "react";
-
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
 const Footer = () => {
   return (
-    <footer className="py-6 bg-gray-800 text-white">
-      <div className="container mx-auto px-6 text-center">
-        <p>© 2024 Your SaaS Project. All rights reserved.</p>
+    <footer className=" bg-gray-800 flex flex-col space-y-10 justify-center p-10">
+      <div className="flex justify-center space-x-8 text-white">
+        <FaFacebook size={30} />
+        <FaLinkedin size={30} />
+        <FaInstagram size={30} />
+        <FaYoutube size={30} />
+        <FaTwitter size={30} />
       </div>
+      <p className="text-center text-white font-medium">
+        &copy; 2022 Company Ltd. All rights reservered.
+      </p>
     </footer>
   );
 };
